b8e6dc9900523a6cf3a59b92ddca6aff9b5c1d05
ststc-sg <service@ststc.com>
2023-05-11 18:55:01 +0200

    task

---
+++ SharpApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace SharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list=new List<int>();
+            list.Add(0);
+            list.Add(1);
+            list.Add(2);
+            var refspan= CollectionsMarshal.AsSpan(list);
+            IcrementInThisThread(refspan);
+
+            foreach(var pt in list)
+                Console.Write(pt);
+            Console.WriteLine();
+            
+            IcrementInOverThread(refspan);
+
+            foreach (var pt in list)
+                Console.Write(pt);
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// modify span in this thread
+        /// </summary>
+        /// <param name="span"></param>
+        static void IcrementInThisThread(Span<int> span)
+        {
+            for(int i=0;i< span.Length;i++)
+                span[i]++;
+        }
+        /// <summary>
+        /// modify span in pool thread
+        /// </summary>
+        /// <param name="span"></param>
+        static void IcrementInThread(Span<int> span)
+        {
+            Task.Run(() => IcrementInThisThread(span)).Wait();
+        }
+    }
+}
